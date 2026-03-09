4b822c0f47d6f56a5468ab7f9cbb49664b44e1bb
samuelkevinga-cpu <samuelkevinga@gmail.com>
2026-03-09 17:48:35 -0600

    This is the work for abstraction thing

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,27 @@
 using System;
-
+using System.Reflection;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2027;
+        job1._endYear = 2030;
+        job1.Display();
+       
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Program Manager";
+        job2._startYear = 2024;
+        job2._endYear = 2027;
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume._jobs[0].Display();
     }
 }
---
+++ week02/Resumes/resume.cs
@@ -0,0 +1,9 @@
+public class Resume
+{
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+    }
+}
